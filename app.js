ad674af52e1970d76ca32d39ee0af6ccf900ff33
Koraboevvv <bexruzkarabaev@gmail.com>
2025-05-03 21:05:53 +0500

    ooo

--- app.js
+++ app.js
@@ -76,3 +76,16 @@ function randomPhoneNumber () {
         clearInterval(interval)
     },2500 )
 }
+
+
+let count = 0;
+
+        function incrementCounter() {
+            count++;
+            document.getElementById('counter').innerText = count;
+        }
+
+        function resetCounter() {
+            count = 0;
+            document.getElementById('counter').innerText = count;
+        }
